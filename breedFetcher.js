3a275e55a2897bc772f64aef62adf2266cf06d04
DevendraChitanna <devendrachitanna@gmail.com>
2021-12-03 00:50:21 -0500

    completed breedFetcher

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,24 +1,28 @@
 const request = require('request');
 
-const breed = process.argv[2];
+
+
+const fetchBreedDescription = function (breedName, callback) {
+
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+
+    if (error) {
+      // console.log("ERROR ERROR", error);
+      return callback(error, null);
+    }
+    //PARSED
+    const [data] = JSON.parse(body);
 
 
 
+    if (!data) {
+      console.log("breed was not found");
+      return callback("breed was not found");
+    }
+    // console.log(data[0].description);
+    return callback(null, data.description)
+  });
+};
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, response, body) => {
+module.exports = { fetchBreedDescription };
 
-  if (error) {
-    console.log("ERROR ERROR", error);
-    return;
-  }
-  //PARSED
-  const data = JSON.parse(body);
-
-
-
-  if (data.length === 0) {
-    console.log(`${breed} was not found`);
-    return;
-  }
-  console.log(data[0].description);
-});
